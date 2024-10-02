e31a95a9263beb53fa3fc1845513e8dc95f4151e
Minyi Zhu <minyizhu@Minyis-MacBook-Pro.local>
2024-10-01 23:47:43 -0700

    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,27 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_backstage_sellin_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_Conjured_quality_degrade_twice(self):
+        items = [Item("Conjured", 2, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(1, items[0].sell_in)
+        self.assertEquals(38, items[0].quality)
+
+    def test_Conjured_quality_degrade_twice_when_negative(self):
+        items = [Item("Conjured", -1, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(-2, items[0].sell_in)
+        self.assertEquals(36, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
